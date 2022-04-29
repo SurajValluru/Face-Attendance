c63d64a354d8a635ab61847dbc1a67ccdf4c09b2
DeadPooL <80475876+SurajValluru@users.noreply.github.com>
2022-04-29 14:46:43 +0530

    Fixes and Added Comments

--- main.py
+++ main.py
@@ -7,16 +7,19 @@ from datetime import datetime
 def encodings(images):
     encodeList = []
     for img in images:
-        encode = face_recognition.face_encodings(img)[0]
+        encode = face_recognition.face_encodings(
+            img)[0]  # Add encodings to the list
         encodeList.append(encode)
     return encodeList
 
 
 def boxTxt(img, name='Unknown', color=(0, 0, 255)):
-    cv2.rectangle(img, (x1*4, y1*4), (x2*4, y2*4), color, 2)
-    cv2.rectangle(img, (x1*4, y2*4-35), (x2*4, y2*4), color, cv2.FILLED)
+    cv2.rectangle(img, (x1*4, y1*4), (x2*4, y2*4),
+                  color, 2)  # Draw the box around face
+    cv2.rectangle(img, (x1*4, y2*4-35), (x2*4, y2*4),
+                  color, cv2.FILLED)  # Below box Label
     cv2.putText(img, name, (x1*4+6, y2*4-6),
-                cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+                cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)  # Label text
 
 
 def attendance(name):
@@ -25,23 +28,24 @@ def attendance(name):
         names = []
         for line in data:
             entry = line.split(',')
-            names.append(entry[0])
+            names.append(entry[0])  # Get all names in record into names list
         if name not in names:
             present = datetime.now()
-            date = present.strftime('%H:%M:%S')
-            report.writelines(f'\n{name},{date}')
+            time = present.strftime('%H:%M:%S')
+            # Add name & time into record
+            report.writelines(f'\n{name},{time}')
 
 
 path = 'known/'
 images = []
 people = []
-known_faces = os.listdir(path)
-print("Files found: ")
+known_faces = os.listdir(path)  # Get files list
 for person in known_faces:
     cImg = cv2.imread(path+person)
-    images.append(cImg)
+    images.append(cImg)  # Load images into list
+    # Get name of file without extension
     people.append(os.path.splitext(person)[0])
-print(known_faces)
+print(people)
 
 try:
     knownEncodings = encodings(images)
@@ -53,27 +57,29 @@ except:
     print('Unknown Error Faced....')
     exit()
 
-capture = cv2.VideoCapture(0)
+capture = cv2.VideoCapture(0)  # Assign object to camera 0
 
 while True:
+    # Return 2 variables, if capture was success and the captured image
     success, img = capture.read()
     if success:
-        check = True
-        name = 'Unknown'
+        check = True  # Counter to find if known face found or not
+        name = 'Unknown'  # Define name to default
+        # Scale down to reduce system burden
         imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
         face = face_recognition.face_locations(imgS)
         encoded_face = face_recognition.face_encodings(imgS)
 
-        for enFc, fcLoc in zip(encoded_face, face):
+        for enFc, fcLoc in zip(encoded_face, face):  # For loop to find multiple faces
             matches = face_recognition.compare_faces(knownEncodings, enFc)
             face_distance = face_recognition.face_distance(
                 knownEncodings, enFc)
             for i in range(len(face_distance)):
                 if min(face_distance) == face_distance[i]:
-                    matchIndex = i
+                    matchIndex = i  # Find corresding matched faces index
             y1, x2, y2, x1 = fcLoc
             if matches[matchIndex]:
-                name = people[matchIndex].upper()
+                name = people[matchIndex]
                 boxTxt(img, name, (0, 255, 0))
             else:
                 boxTxt(img)
@@ -82,11 +88,11 @@ while True:
             if name != 'Unknown':
                 attendance(name)
 
-        key = cv2.waitKey(1)
+        key = cv2.waitKey(1)  # To record key strokes
 
-        if key == 27:
+        if key == 27:  # If key is 'esc' key
             break
-        if len(face) != 0:
+        if len(face) != 0:  # If any face is found
             cv2.imshow('Cam', img)
         else:
             cv2.destroyAllWindows()
